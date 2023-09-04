340fbc794d8c3aaede68845b0a49f0306a2c083d
Darya Kutovaya <kutovayadarya@gmail.com>
2023-09-04 09:44:38 -0700

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const cors = require("cors")({ origin: true });
+const admin = require("firebase-admin");
+const { onRequest } = require("firebase-functions/v2/https");
+
+const serviceAccount = require("./climateappfb-firebase-adminsdk-kcd39-8a63bc8cfe.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+exports.zips = onRequest((request, response) => {
+
+    cors(request, response, async () => {
+        
+        try {
+
+            const zip = request.query.zip;
+        
+            const snapshot = await admin.firestore().doc(`zips/${zip}`).get();
+        
+            const data = snapshot.data();
+        
+            response.status(200).send(data);
+        
+        } catch (error) {
+        
+            response.status(500).send(error);
+        
+        }
+
+    });
+
+});
