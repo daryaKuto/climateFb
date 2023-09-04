69ec6697c61e13facfb9e7180562be8c5aac135e
Darya Kutovaya <kutovayadarya@gmail.com>
2023-09-04 14:44:20 -0700

    components and css

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,28 @@
 const cors = require("cors")({ origin: true });
 const admin = require("firebase-admin");
 const { onRequest } = require("firebase-functions/v2/https");
+const fetch = require("node-fetch");
 
 const serviceAccount = require("./climateappfb-firebase-adminsdk-kcd39-8a63bc8cfe.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
+
 
 exports.zips = onRequest((request, response) => {
+  cors(request, response, async () => {
+    try {
+      const zip = request.query.zip;
 
-    cors(request, response, async () => {
-        
-        try {
+      const snapshot = await admin.firestore().doc(`zips/${zip}`).get();
 
-            const zip = request.query.zip;
-        
-            const snapshot = await admin.firestore().doc(`zips/${zip}`).get();
-        
-            const data = snapshot.data();
-        
-            response.status(200).send(data);
-        
-        } catch (error) {
-        
-            response.status(500).send(error);
-        
-        }
+      const data = snapshot.data();
 
-    });
-
+      response.status(200).send(data);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
 });
+
